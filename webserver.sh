bfab999ca6dd05ce059e0ae31eaf9294cc3b48ab
Shubhampujari11 <122546945+Shubhampujari11@users.noreply.github.com>
2023-05-29 19:37:10 +0530

    Update webserver.sh for jenkins

--- webserver.sh
+++ webserver.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo -e "Changing or creating index.html..."
-echo -e "THIS IS NEW CONTENT @ `date` !!!" > index.html
+echo -e "JENKINS DID THIS...@ `date` !!!" > index.html
 mv index.html /var/www/html/
 echo -e "Created new HTML page"
 echo -e check on "http://3.110.187.36:80"
